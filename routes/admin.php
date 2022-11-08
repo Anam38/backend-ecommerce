7a470923d5ef7ab84a2f1b7e8009b103bb72df1e
an6 <namanam38@gmail.com>
2022-11-08 13:59:38 +0700

    test

--- routes/admin.php
+++ routes/admin.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\Web\FrontIntegrationController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes TEST
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
